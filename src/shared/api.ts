2e503758eb9b5b466dc0e782c543cec8b9961538
MSI\jonbr <cultpodcasts@educocult.com>
2026-03-06 20:22:05 +0000

    refactor: remove unused InternalEndpoint and type definitions from api.ts

--- src/shared/api.ts
+++ src/shared/api.ts
@@ -1,7 +1,1 @@
-export const InternalEndpoint = {
-  OnPostCreate: "/internal/menu/post-create",
-  OnAppInstall: "/internal/on-app-install",
-} as const;
-
-export type InternalEndpoint =
-  (typeof InternalEndpoint)[keyof typeof InternalEndpoint];
+export {};
